b3b6b1aaa8452abe663598e45b631c563a9e0f74
Ferney Niño <Ferney Nio@DESKTOP-91UIOGN.home>
2022-07-19 23:56:11 -0500

    Semana1 Sesion1 Mintic 2022 Java

---
+++ src/holamundoxconsole/HolaMundoxConsole.java
@@ -0,0 +1,14 @@
+
+
+package holamundoxconsole;
+
+
+public class HolaMundoxConsole {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("Hola Mi Prro");
+    }
+
+}
